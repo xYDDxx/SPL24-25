24988a8816e1865f766a9374177c751b28651db0
Ian <ianlee.felder@gmail.com>
2024-12-10 12:10:20 +0100

    Cards with working CMS

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,50 +1,40 @@
+import { useState, useEffect } from "react";
 import Card from "./Card";
 
-type Props = {};
+type Person = {
+  id?: number;
+  firstname: string;
+  lastname: string;
+  image: string;
+  birthday: string;
+};
 
-export default function CardContainer({}: Props) {
+export default function CardContainer() {
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    fetch("http://10.115.1.35:8055/items/people").then((response) =>
+      response.json().then((data: any) => {
+        setPeople(data.data);
+      })
+    );
+  }, []);
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 max-w-8xl">
-      <Card
-        image="https://picsum.photos/300/200?random=1"
-        name="Riedmann Andres"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=2"
-        name="Morit"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=3"
-        name="Kronabeiter"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=4"
-        name="Luggi"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=5"
-        name="Jan Fehler"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=6"
-        name="Noel Fielmann"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=7"
-        name="Diego Knechtner"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
-      <Card
-        image="https://picsum.photos/300/200?random=8"
-        name="Ronny Oppman"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac volutpat leo. Etiam ac nunc eget ligula dapibus."
-      />
+      {people.map((person) => (
+        <Card
+          key={person.id}
+          image={`http://10.115.1.35:8055/assets/${person.image}`}
+          name={`${person.firstname} ${person.lastname}`}
+          description={`Birthday: ${formatDate(person.birthday)}`}
+        />
+      ))}
     </div>
   );
 }
